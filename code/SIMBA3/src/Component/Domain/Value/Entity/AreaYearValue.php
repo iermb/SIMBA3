cfafe4fd9c0c75057889c00c58c11c5045f6dfbe
Jordi Llobet <jordi.llobet@uab.cat>
2021-01-13 14:01:32 +0100

    Add AreaYearValue entity

---
+++ code/SIMBA3/src/Component/Domain/Value/Entity/AreaYearValue.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace SIMBA3\Component\Domain\Value\Entity;
+
+
+class AreaYearValue
+{
+    private int $indicatorId;
+    private int $areaId;
+    private int $areaNameId;
+    private int $year;
+    private float $value;
+    private bool $isPublic;
+    private string $noteValue;
+
+}
