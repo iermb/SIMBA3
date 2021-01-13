3c1bf30a343bcc94294dfed32cb760f6632fb316
Jordi Llobet <jordi.llobet@uab.cat>
2021-01-13 10:20:38 +0100

    Add TypeIndicator and their mapping

---
+++ code/SIMBA3/src/Component/Domain/Indicator/Entity/TypeIndicator.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace SIMBA3\Component\Domain\Indicator\Entity;
+
+
+class TypeIndicator
+{
+    private int $id;
+    private bool $area;
+    private bool $year;
+    private bool $month;
+    private int $numIndependentVars;
+    
+}
