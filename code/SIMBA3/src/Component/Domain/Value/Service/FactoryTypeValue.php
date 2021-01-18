46205eaf175af4c9dc6ebab65a59b1f59bc9d2a8
metathairs <Daniel.Martinez.Bou@uab.cat>
2021-01-18 10:38:42 +0100

    factory updated with year-value

--- code/SIMBA3/src/Component/Domain/Value/Service/FactoryTypeValue.php
+++ code/SIMBA3/src/Component/Domain/Value/Service/FactoryTypeValue.php
@@ -5,14 +5,19 @@ namespace SIMBA3\Component\Domain\Value\Service;
 
 
 use SIMBA3\Component\Domain\Value\Repository\AreaYearValueRepository;
+use SIMBA3\Component\Domain\Value\Repository\YearValueRepository;
 
 class FactoryTypeValue
 {
     private AreaYearValueRepository $areaYearValueRepository;
+    private YearValueRepository $yearValueRepository;
 
-    public function __construct(AreaYearValueRepository $areaYearValueRepository)
-    {
+    public function __construct(
+        AreaYearValueRepository $areaYearValueRepository,
+        YearValueRepository $yearValueRepository,
+    ) {
         $this->areaYearValueRepository = $areaYearValueRepository;
+        $this->yearValueRepository = $yearValueRepository;
     }
 
     public function getObjectTypeValue(string $objectType): TypeValue
@@ -20,6 +25,10 @@ class FactoryTypeValue
        switch ($objectType) {
             case "AREA_YEAR_VALUE":
                 return new AreaYearTypeValue($this->areaYearValueRepository);
+
+            case "YEAR_VALUE":
+                return new YearTypeValue($this->yearValueRepository);
+
             default:
                 throw new \InvalidArgumentException("Not exists the type value");
         }
